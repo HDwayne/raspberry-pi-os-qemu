694019edc2b76e877a5b72cea2beceef8431ad19
HDwayne <hdwayne2002@gmail.com>
2024-09-24 17:15:47 +0200

    moving lesson02 to my env

--- src/lesson01/src/kernel.c
+++ src/lesson01/src/kernel.c
@@ -1,9 +1,17 @@
+#include "printf.h"
+#include "utils.h"
 #include "mini_uart.h"
 
 void kernel_main(void)
 {
 	uart_init();
-	uart_send_string("Kernel initialized\n");
+	uart_send_string("UART is working \r\n");
+
+	init_printf(0, putc);
+	printf("PRINTF is working \r\n");
+
+	int el = get_el();
+	printf("Exception level: %d \r\n", el);
 
 	while (1) {
 		uart_send(uart_recv());
